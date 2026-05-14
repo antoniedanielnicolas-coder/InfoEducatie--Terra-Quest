db2fd6af72b72caa62798a9e70fa3ba7a4ad8221
George <georgem16122009@gmail.com>
2026-05-15 01:15:28 +0300

    nanu

---
+++ infoeducatieee/public/welcome-overlay-init.js
@@ -0,0 +1,11 @@
+﻿import { initWelcomeOverlay } from './welcome-overlay.js';
+
+function init() {
+  initWelcomeOverlay();
+}
+
+if (document.readyState === 'loading') {
+  document.addEventListener('DOMContentLoaded', init);
+} else {
+  init();
+}
